4d8e904285673d2900e9d41b35b00adf00ab172a
geetads <45489768+geetads@users.noreply.github.com>
2020-01-08 23:17:25 -0500

    Update ListBooks.js

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -21,7 +21,12 @@ return (
             <li key={i}>
                        <div className="book">
                           <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}></div>
+                             {book.imageLinks ? (
+                            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
+                          ):
+                            <div className="book-cover" style={{ width: 128, 
+                              height: 193, 
+                                backgroundImage: `url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")`}}></div>}
                             <div className="book-shelf-changer">
                               <select value={book.shelf} onChange={(e)=>handleSelectChange(book,e)}>
                                 <option value="none" disabled>Move to...</option>
